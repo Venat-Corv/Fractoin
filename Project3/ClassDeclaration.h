4a6b7bf5ff822bf190fb3a0b9061cdc173ba7817
Student <Student@304-2>
2019-07-17 08:57:59 +0300

    on 3 files

---
+++ Project3/ClassDeclaration.h
@@ -0,0 +1,43 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#define delimiter "\n-------------------------------------\n"
+
+//#define CONSTRUCTORS_CHECK
+
+class String;
+String operator+(const String& left, const String& right);
+
+class String
+{
+	int size;//Размер строки 
+	char* str;//Адрес строки в динамической памяти
+public:
+	int get_size() const;
+	const char* get_str() const;
+	char* get_str();
+
+
+	//       Constructors:
+	String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	String(String&& other);
+	~String();
+
+	//       Operators:
+
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+
+	String& operator+=(const String& other);
+
+	const char& operator[](int i)const;
+	char& operator[](int i);
+
+	//       Methods:
+	void print();
+};
+
+ostream& operator<<(ostream& os, const String& obj);
+String operator+(const String& left, const String& right);
